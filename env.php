414317ec8f827b04bd7434af59f5dcd22fec15f9
BUI THIN LONG <longtb321@gmail.com>
2023-10-28 11:07:28 +0700

    src

--- env.php
+++ env.php
@@ -4,7 +4,7 @@ const DBUSER = "root";
 const DBPASS = "";
 const DBCHARSET = "utf8";
 const DBHOST = "127.0.0.1";
-const BASE_URL = "http://localhost/ass2/";
+const BASE_URL = "http://localhost/duan1/";
 
 function url($url){
     return BASE_URL.$url;
